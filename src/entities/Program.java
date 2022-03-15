89e521f1f1e9b7c485573b369195b6f78ebc4844
Marcelo Soares <marcelossoares@live.com>
2022-03-14 21:52:04 -0300

    class Position

--- src/entities/Program.java
+++ src/entities/Program.java
@@ -1,10 +1,14 @@
 package entities;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+			Position pos  = new Position(2,3);
+			System.out.println(pos);
 	}
 
 }
